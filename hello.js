87a5ad94afcabe281144cabcba4d9d47c4d4d1e0
bleepbloopsify <bleepbloopsify@gmail.com>
2015-11-01 20:14:31 -0500

    finished 2 challenges from node class

---
+++ hello.js
@@ -0,0 +1,24 @@
+//Challenge: My first async io!
+var fs = require('fs')
+
+
+var lines = file.split('\n').length - 1
+console.log(lines)
+
+
+
+// //Challenge: Basic I/O
+// var fs = require('fs')
+//
+// var filebuffer = fs.readFileSync(process.argv[2]).toString()
+// console.log(filebuffer.split('\n').length - 1)
+
+
+
+//Challenge : Baby Steps
+// var n = 0;
+// for (var i = 2; i < process.argv.length; i++){
+//   n += Number(process.argv[i]);
+// }
+//
+// console.log(n)
