d6d9efdc253e5001309d476c8f3e37d79abbc6ee
bleepbloopsify <bleepbloopsify@gmail.com>
2015-11-11 23:56:08 -0500

    pls

--- hello.js
+++ hello.js
@@ -1,9 +1,36 @@
+//Challenge Make it Modular!!
+// var mine = require('./mine.js')
+// console.log( mine(process.argv[2], process.argv[3]) );
+//
+//
+
+
+//Challenge Filtered ls
+// var fs = require('fs');
+// fs.readdir(process.argv[2], function (err, files){
+//   if (err) throw err;
+//   for (i = 0; i < files.length; i++){
+//     if(files[i].split('.')[1] == process.argv[3]){
+//         console.log(files[i]);
+//     }
+//   }
+// });
+//
+//
+
+  
 //Challenge: My first async io!
-var fs = require('fs')
+// var fs = require('fs');
+// var lines = 0;
+// fs.readFile(process.argv[2], 'utf8', function ( err, data ){
+//   if (err) throw err;
+//   lines = data.split('\n').length - 1;
+//   console.log(lines);
+// });
 
 
-var lines = file.split('\n').length - 1
-console.log(lines)
+// var lines = file.split('\n').length - 1;
+// console.log(lines);
 
 
 
---
+++ mine.js
@@ -0,0 +1,16 @@
+// var fs = require('fs')
+//
+// exports.filter =
+
+
+
+//   var fs = require('fs');
+//   fs.readdir(process.argv[2] , function (err, files){
+//     if (err) throw err;
+//     for (i = 0; i < files.length; i++){
+//       if( files[i].split('.')[1] == process.argv[3] ){
+//         callback( null, ( files[i] ) ) ;
+//       }
+//     }
+//   });
+// }
